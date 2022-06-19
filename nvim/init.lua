a034900db605215823d3423e651871ab4ebdd7b6
Mario Merlano <mario-merlano@hotmail.com>
2022-06-19 01:14:44 -0300

    feat: add packer, tree-sitter & colorscheme

---
+++ nvim/init.lua
@@ -0,0 +1,39 @@
+local cmd = vim.cmd
+local g = vim.g
+local opt = vim.opt
+
+g.mapleader = " "
+
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+  
+  -- Languages parser
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Colorscheme
+  use 'marko-cerovac/material.nvim'
+
+end)
+
+-- Tree-sitter config
+require'nvim-treesitter.configs'.setup {
+  -- Languages to parse 
+  ensure_installed = { "lua", "javascript", },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  highlight = {
+    enable = true,
+  },
+}
+
+vim.g.material_style = "darker"
+vim.cmd 'colorscheme material'
+
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+
